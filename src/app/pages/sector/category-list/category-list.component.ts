1ca6884676f2461065ab6bd9cfe11ecb57eb139f
Willian <willianmazzorana@hotmail.com>
2021-11-28 12:01:54 -0300

    listagem de categoria

--- src/app/pages/sector/category-list/category-list.component.ts
+++ src/app/pages/sector/category-list/category-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { CategoryService } from './../../../services/category.service';
+import { Icategory } from './../../../models/icategory';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-category-list',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  categories: Icategory[];
+  category: Icategory;
+  sectorId: number;
+  success: boolean;
+  message: string;
+  modalRef?: BsModalRef;
+  pag : number = 1 ;
+  contador : number = 5;
+
+  constructor(
+    private categoryService: CategoryService,
+    private modalService: BsModalService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  listAll(){
+    this.categoryService.getAll().subscribe(
+      (respose) => {
+        this.categories = respose;
+      }
+    );
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
   }
 
 }
