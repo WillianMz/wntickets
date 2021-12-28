7b2a34948bd2ec9736fb7e6e62803ab8d750a79d
Willian Mazzorana <willianmazzorana@hotmail.com>
2021-12-28 00:04:44 -0300

    Removido funcionalidades que nao serao mais utilizadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
-import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'auth',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
@@ -13,33 +12,23 @@ const routes: Routes = [
     loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule)
   },
   {
-    path: 'companies',
-    component: AdminLayoutComponent,
-    loadChildren: () => import('./pages/company/company.module').then(m => m.CompanyModule)
-  },
-  {
     path: 'home',
-    component: AdminLayoutComponent,
     loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
   },
   {
     path: 'sectors',
-    component: AdminLayoutComponent,
     loadChildren: () => import('./pages/sector/sector.module').then(m => m.SectorModule)
   },
   {
     path: 'settings',
-    component: AdminLayoutComponent,
     loadChildren: () => import('./pages/settings/settings.module').then(m => m.SettingsModule)
   },
   {
     path: 'tickets',
-    component: AdminLayoutComponent,
     loadChildren: () => import('./pages/ticket/ticket.module').then(m => m.TicketModule)
   },
   {
     path: 'users',
-    component: AdminLayoutComponent,
     loadChildren: () => import('./pages/user/user.module').then(m => m.UserModule)
   }
 ];
--- src/app/models/icompany.ts
+++
@@ -1,9 +0,0 @@
-export interface Icompany {
-  id?: number;
-  razaoSocial?: string;
-  nomeFantasia?: string;
-  cnpj?: string;
-  email?: string;
-  ativa?: boolean;
-  nickname?: string;
-}
