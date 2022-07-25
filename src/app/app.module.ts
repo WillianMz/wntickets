7db59b4cde8a62a01ec240a73a6bb59ecc698d6f
Willian <willianmazzorana@hotmail.com>
2022-07-24 23:35:53 -0300

    Menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxPopperjsModule } from 'ngx-popperjs';
+import { AdminComponent } from './layouts/admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -12,7 +12,7 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.titlePage = "SUPORTE";
+    this.titlePage = "wntickets";
   }
 
 }
