25043a77f9ec2e50b5242f99a9023f393ca70bf7
Luciano de Azevedo <luk.azvdo@gmail.com>
2022-09-17 13:03:48 -0300

    Equip - Validators

--- src/app/modules/equipment/equip-form/equip-form.component.ts
+++ src/app/modules/equipment/equip-form/equip-form.component.ts
@@ -196,7 +196,7 @@ export class EquipFormComponent implements OnInit {
       codInterno: new FormControl(equip.codInterno, [ 
         Validators.required,
         Validators.minLength(2),
-        Validators.maxLength(40)
+        Validators.maxLength(15)
       ]),
       tipo: new FormControl(equip.tipo?.id, [Validators.required]),
       setor: new FormControl(equip.setor?.id, [Validators.required]),
@@ -208,12 +208,12 @@ export class EquipFormComponent implements OnInit {
       descricao: new FormControl(equip.descricao, [
         Validators.required,
         Validators.minLength(2),
-        Validators.maxLength(40)
+        Validators.maxLength(100)
       ]),
       fabricante: new FormControl(equip.fabricante, [
         Validators.required,
         Validators.minLength(2),
-        Validators.maxLength(40)
+        Validators.maxLength(50)
       ]),
       marca: new FormControl(equip.marca, [
         Validators.required,
@@ -228,7 +228,7 @@ export class EquipFormComponent implements OnInit {
       numSerial: new FormControl(equip.numSerial, [
         Validators.required,
         Validators.minLength(2),
-        Validators.maxLength(40)
+        Validators.maxLength(15)
       ]),
       anoFabricacao: new FormControl(equip.anoFabricacao, [
         Validators.required,
@@ -241,8 +241,16 @@ export class EquipFormComponent implements OnInit {
         Validators.maxLength(10)
       ]),
       valorCompra: new FormControl(equip.valorCompra, [Validators.required]),
-      anotacoes: new  FormControl(equip.anotacoes),
-      motivoBaixa: new FormControl(equip.motivoBaixa)
+      anotacoes: new  FormControl(equip.anotacoes, [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(100)
+      ]),
+      motivoBaixa: new FormControl(equip.motivoBaixa, [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(100)
+      ])
     });
   }
 
