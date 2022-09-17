f4d3ff1de4edfe752b6d44ea78e3a7beaeca94d4
Luciano de Azevedo <luk.azvdo@gmail.com>
2022-09-17 16:39:49 -0300

    Chamados - List Init

--- src/app/services/ticket.service.ts
+++ src/app/services/ticket.service.ts
@@ -9,11 +9,36 @@ import { environment } from 'src/environments/environment';
 })
 export class TicketService {
 
-  private url = `${environment.api}/ticket`;
+  /* private url = `${environment.api}/ticket`; */
 
   constructor(private http: HttpClient) { }
 
-  public getAll(): Observable<TicketModel[]> {
-    return this.http.get<TicketModel[]>(this.url);
+  save(ticket: TicketModel){
+    if(ticket.id){
+      console.log(ticket);
+      return this.update(ticket);
+    }
+    else {
+      return this.create(ticket);
+    }
   }
+
+  private create(ticket: TicketModel){
+    return this.http.post(`${environment.api}/Ticket`, ticket);
+  }
+
+  private update(iticket: TicketModel){
+    return this.http.put(`${environment.api}/Ticket`, iticket);
+  }
+
+  public delete(id: number){
+    return this.http.delete(`${environment.api}/Ticket/${id}`);
+  }
+
+  public getAll(): Observable<TicketModel[]> {
+    return this.http.get<TicketModel[]>(`${environment.api}/Ticket`)
+  }
+
+
+
 }
